cb7d794d21fdeee3218e3e31d17a22767b49f01d
Celestine Okolie <stinekene@gmail.com>
2016-08-19 04:50:12 +0100

    Removed line 42

--- noteApplication.js
+++ noteApplication.js
@@ -39,7 +39,6 @@ var note = new NotesApplication('Achabe');
 note.create('This Fall Apart');
 note.create('Oliver Twist');
 note.create('No Second Chance');
-note.create('Second Chance');
 
 
 note.listNotes();
